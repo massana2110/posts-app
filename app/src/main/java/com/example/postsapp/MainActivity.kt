2c5d5f8b294bbc9addc20bfb4ede29da8701d155
David Massana <00072416@uca.edu.sv>
2022-07-23 23:34:54 -0600

    :sparkles: Add toolbar and fragment nav

--- app/src/main/java/com/example/postsapp/MainActivity.kt
+++ app/src/main/java/com/example/postsapp/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.postsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.postsapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var _binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        setSupportActionBar(_binding.mainToolbar)
     }
 }
--- app/src/main/java/com/example/postsapp/views/posts/PostsFragment.kt
+++ app/src/main/java/com/example/postsapp/views/posts/PostsFragment.kt
@@ -5,16 +5,46 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
 import com.example.postsapp.R
+import com.example.postsapp.databinding.FragmentPostsBinding
 
 class PostsFragment : Fragment() {
+
+    private lateinit var _binding: FragmentPostsBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_posts, container, false)
+        _binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_posts,
+            container,
+            false
+        )
+
+        setupButtonListeners()
+
+        return _binding.root
     }
+
+    private fun setupButtonListeners() {
+        _binding.commentsBtn.setOnClickListener {
+            findNavController().navigate(
+                PostsFragmentDirections.actionPostsFragmentToCommentsFragment()
+            )
+        }
+
+        _binding.albumBtn.setOnClickListener {
+            findNavController().navigate(
+                PostsFragmentDirections.actionPostsFragmentToAlbumFragment()
+            )
+        }
+    }
+
+
 
 }
