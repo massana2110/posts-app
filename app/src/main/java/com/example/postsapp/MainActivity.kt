9b7cea2a27f22f6db59c390f29500d80036a5d03
David Massana <00072416@uca.edu.sv>
2022-07-23 23:48:28 -0600

    :bug: Fix implementation of nav controller
    In activity layout when we are implement ing Fragment Container View, is necessary to use supportFragmentManager.findFragment and cast to NavHostFragment, then access to navController property

--- app/src/main/java/com/example/postsapp/MainActivity.kt
+++ app/src/main/java/com/example/postsapp/MainActivity.kt
@@ -3,6 +3,10 @@ package com.example.postsapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.example.postsapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,5 +18,11 @@ class MainActivity : AppCompatActivity() {
         _binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         setSupportActionBar(_binding.mainToolbar)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+
+        _binding.mainToolbar.setupWithNavController(navController, appBarConfiguration)
     }
 }
